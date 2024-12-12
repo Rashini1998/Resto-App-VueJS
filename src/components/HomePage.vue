3d58d54fa79f0fb12e167f51a4d5a3ea104b9ad0
Rashini Sathsarani Gamalath <gamalathrashini@gmail.com>
2024-12-12 16:26:46 +0530

    Complete Sign Page and Redirections

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -2,8 +2,17 @@
     <h1>Hello User, Welcome to the Home Page!</h1>
 </template>
 <script>
-    export default{
-        name:"HomePage"
+    export default
+    {
+        name:"HomePage",
+        mounted()
+        {
+        let user = localStorage.getItem('user-info');
+            if(!user)
+            {
+                this.$router.push({name:'SignUp'}) 
+            }
+        }
     }
 </script>
 <style></style>
