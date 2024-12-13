c8654d329f26c2f0db3c7ee5b77238af30322ba0
Rashini Sathsarani Gamalath <gamalathrashini@gmail.com>
2024-12-13 13:20:40 +0530

    Add Tailwind css

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,7 +7,7 @@
             </a>
             <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
                 <button v-on:click="logout" type="button"
-                    class="text-white bg-fuchsia-700 border-slate-50 hover:bg-fuchsia-500 hover:text-fuchsia-900 focus:ring-2  focus:ring-fuchsia-300 font-medium rounded-lg text-sm px-4 py-2 text-center ">Logout</button>
+                    class="text-white bg-fuchsia-700 border-slate-50 hover:bg-fuchsia-500 hover:text-black focus:ring-2  focus:ring-fuchsia-300 font-medium rounded-lg text-sm px-4 py-2 text-center ">Logout</button>
                 <button type="button"
                     class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                     aria-controls="navbar-cta" aria-expanded="false">
