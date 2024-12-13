b304dea206bc38036b72004c69798a7bd2f5f84c
Rashini Sathsarani Gamalath <gamalathrashini@gmail.com>
2024-12-13 09:06:38 +0530

    Add Header

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="nav">
+        <a href="#">Home</a>
+        <a href="#">Add Restaurant</a>
+        <a href="#">Update Restaurant</a>
+        <a href="#">Logout</a>
+    </div>
+</template>
+<script>
+export default {
+    name: "PageHeader"
+}
+</script>
+<style>
+.nav{
+    background-color: purple;
+    overflow: hidden;
+}
+.nav a{
+    float: left;
+    color: aliceblue;
+    padding: 14px 16px;
+    text-align: center;
+    font-size: 17px;
+    text-decoration: none;
+    margin-right: 5px;
+}
+.nav a:hover{
+    background-color: rgb(222, 134, 222);
+    color: purple;
+}
+</style>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,10 +1,15 @@
 <template>
+    <PageHeader/>
     <h1>Hello User, Welcome to the Home Page!</h1>
 </template>
 <script>
+import PageHeader from './Header.vue';
     export default
     {
         name:"HomePage",
+        components:{
+            PageHeader
+        },
         mounted()
         {
         let user = localStorage.getItem('user-info');
