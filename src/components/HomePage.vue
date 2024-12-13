a5fbbcae640667cba8b2103785967cd26e5e6a36
Rashini Sathsarani Gamalath <gamalathrashini@gmail.com>
2024-12-13 11:47:13 +0530

    List of restaurant with API

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,9 +1,32 @@
 <template>
     <PageHeader/>
     <h1>Hello {{name}}, Welcome to the Home Page!</h1>
+    <table border="1px">
+        <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Contact</th>
+            <th>Address</th>
+        </tr>
+        <tr v-for="item in restuarant" :key="item.id">
+            <td>
+                {{ item.id }}
+            </td>
+            <td>
+                {{ item.name }}
+            </td>
+            <td>
+                {{ item.contact }}
+            </td>
+            <td>
+                {{ item.address }}
+            </td>
+        </tr>
+    </table>
 </template>
 <script>
 import PageHeader from './Header.vue';
+import axios from 'axios';
     export default
     {
         name:"HomePage",
@@ -13,10 +36,11 @@ import PageHeader from './Header.vue';
         data()
         {
             return{
-                name:''
+                name:'',
+                restuarant:[],
             }
         },
-        mounted()
+        async mounted()
         {
         let user = localStorage.getItem('user-info');
         this.name=JSON.parse(user).name;
@@ -24,7 +48,15 @@ import PageHeader from './Header.vue';
             {
                 this.$router.push({name:'SignUp'}) 
             }
+            let result = await axios.get("http://localhost:3000/restuarant");
+            this.restuarant=result.data;
         }
     }
 </script>
-<style></style>
+<style>
+    td{
+        width:160px;
+        height: 40px;
+
+    }
+</style>
